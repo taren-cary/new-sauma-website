e9a975036b9972eb9e533346138c1d6aff55d6b5
Taren Cary <saumagalactic@gmail.com>
2025-09-17 14:37:30 -0400

    Fixed Meta Pixel

--- src/pages/Mercury.jsx
+++ src/pages/Mercury.jsx
@@ -51,27 +51,28 @@ const Container = styled.div`
 const Mercury = () => {
   return (
     <>
-      <Helmet>
+            <Helmet>
         <title>Mercury - AI Receptionist by Sauma AI</title>
         <meta name="description" content="Mercury will book you more appointments, enrich more of your leads, & make deeper connections, all with human-like emotion - autonomously." />
-        <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '758182653800031');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=758182653800031&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-        
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '758182653800031');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=758182653800031&ev=PageView&noscript=1" />'
+          }}
+        />
       </Helmet>
       
       <Navbar />
